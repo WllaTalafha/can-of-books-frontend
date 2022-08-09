dfea465953aab0df13c0715822cf6f8153997afc
Wlla Talafha <wlla.issa396@gmail.com>
2022-08-09 20:51:41 +0300

    done

--- src/Header.js
+++ src/Header.js
@@ -8,11 +8,11 @@ class Header extends React.Component {
   render() {
     return (
         <div>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="light">
-          <Navbar.Brand style={{color:'white'}}> My Favorite Books </Navbar.Brand>
-          <NavItem ><Link to="/" className="nav-link" style={{color:'white'}} >Home &nbsp;</Link></NavItem>
-          <NavItem ><Link to="/profile" className="nav-link"  style={{color:'white'}} >Profile&nbsp;&nbsp;</Link></NavItem>
-          <NavItem ><Link to="/about" className="nav-link" style={{color:'white'}} >About&nbsp;&nbsp;</Link></NavItem>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+          <Navbar.Brand style={{color:'black'}}> My Favorite Books </Navbar.Brand>
+          <NavItem ><Link to="/" className="nav-link" style={{color:'black'}} >Home &nbsp;</Link></NavItem>
+          <NavItem ><Link to="/profile" className="nav-link"  style={{color:'black'}} >Profile&nbsp;&nbsp;</Link></NavItem>
+          <NavItem ><Link to="/about" className="nav-link" style={{color:'black'}} >About&nbsp;&nbsp;</Link></NavItem>
           <NavItem ><br/> <AuthButtons /> </NavItem>
           {/* PLACEHOLDER: render a navigation link to the about page */}
         </Navbar>
